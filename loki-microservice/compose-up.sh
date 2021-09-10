a976da58a4ac41c2b1f7d1a5cea89994dfac7631
Trevor Whitney <trevorjwhitney@gmail.com>
2021-09-10 12:10:13 -0600

    add loki microservice env
    Signed-off-by: Trevor Whitney <trevorjwhitney@gmail.com>

---
+++ loki-microservice/compose-up.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
+SRC_DEST="${SCRIPT_DIR}/.src/"
+# sync all sources for dlv
+rm -Rf "${SRC_DEST}"
+mkdir "${SRC_DEST}"
+for d in cmd pkg vendor clients
+do
+    cp -Rf "${SCRIPT_DIR}/../../loki/${d}/" "${SRC_DEST}/${d}/"
+done
+
+# build loki -gcflags "all=-N -l" disables optimizations that allow for better run with combination with Delve debugger.
+pushd "${SCRIPT_DIR}/../../loki"
+  CGO_ENABLED=0 GOOS=linux go build -mod=vendor -gcflags "all=-N -l" -o "${SCRIPT_DIR}/loki" "./cmd/loki"
+popd
+
+# ## install loki driver to send logs
+docker plugin install grafana/loki-docker-driver:latest --alias loki-compose --grant-all-permissions || true
+# build the compose image
+docker-compose -f "${SCRIPT_DIR}"/docker-compose.yml build distributor
+# cleanup sources
+rm -Rf "${SRC_DEST}"
+
+
+set +e
+op list items 2>&1 > /dev/null                                                                                                                                                                                                                             [11:11:32]
+if [[ $? -ne 0 ]]; then
+  eval "$(op signin my)"
+fi
+set -e
+
+grafana_com_logs=$(op get item grafana-com-logs | jq '.details.sections | .[].fields')
+logs_hostname=$(echo "$grafana_com_logs" | jq -r '.[] | select(.n == "hostname") | .v')
+logs_user=$(echo "$grafana_com_logs" | jq -r '.[] | select(.n == "username") | .v')
+logs_token=$(echo "$grafana_com_logs" | jq -r '.[] | select(.n == "credential") | .v')
+
+LOGS_URL="https://${logs_user}:${logs_token}@${logs_hostname}/loki/api/v1/push" docker-compose -f "${SCRIPT_DIR}"/docker-compose.yml up "$@"
+
+for dir in "${SCRIPT_DIR}/.data-compactor" \
+    "${SCRIPT_DIR}/.data-ingester-1" \
+    "${SCRIPT_DIR}/.data-ingester-2" \
+    "${SCRIPT_DIR}/.data-minio" \
+    "${SCRIPT_DIR}/.data-querier"; do
+  sudo rm -Rf "$dir"
+done
--- loki-ssd/up.sh
+++ loki-ssd/up.sh
@@ -3,3 +3,6 @@
 docker-compose -f docker-compose.yaml up \
   --force-recreate \
   --remove-orphans 2>&1 | lnav -t
+
+# this will wait until lnav has been quit
+docker-compose -f docker-compose.yaml down --remove-orphans
